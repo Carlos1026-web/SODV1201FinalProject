5a4e5a6780de5c9ef2f40388cde70b3cda53af8e
Carlos1026-web <Derek.carlos04@gmail.com>
2025-03-19 22:03:53 -0600

    updated log in page, not fully working and added defer to all html files

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const propertyWorkspaceArray = [];
 
 const usersArray = [];
 
+const currentUser = "";
+
 $("#signUpForm").on("submit", function() {
     let email = document.getElementById("userEmail").value ;
     let password = document.getElementById("userPassword").value;
@@ -31,4 +33,27 @@ $("#signUpForm").on("submit", function() {
     alert(`${newUser.email}\n${newUser.password}\n${newUser.name}\n${newUser.phone}\n${newUser.role}`);
 
     $("#signUpMsg").html("Sign Up Success!");
+});
+
+$("#logInForm").on("submit", function() {
+    let logInEmail = document.getElementById("logInUserEmail").value ;
+    let logInPassword = document.getElementById("logInPassword").value;
+
+    if(usersArray.length > 0) {
+
+        for(let i = 0; i < usersArray.length; i++) {
+            if(logInEmail == usersArray[i].email && logInPassword == usersArray[i].password) {
+
+            }
+            else if (logInPassword != usersArray[i].password) {    //debugging purposes ONLY
+                alert("Password is not correct!");
+            }
+            else {
+                alert("Incorrect Credentials");
+            }
+        }
+    }
+    else {
+        alert("No users in the database!")
+    }
 });
