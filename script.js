10d4aca5d3d65ed542a28675dafa4d970abf5558
Carlos1026-web <Derek.carlos04@gmail.com>
2025-03-21 21:32:30 -0600

    building the create table function so that it the displayed list can be updated in real time

--- script.js
+++ script.js
@@ -27,6 +27,9 @@ function loadUserNum() {
 function loadName() {   // to be used on coworker/owner page on properties
     loadUserNum() ;
     $('#coWorkerIntro').text(`Hello, ${currentUser}!`);
+
+    createTable();
+    $(".tableDiv").append(tableString);
 }
 
 $("#signUpForm").on("submit", function() {
@@ -134,3 +137,46 @@ $("#newPlaceForm").on("submit", function() {
 
     $("#signUpMsg").html("Sign Up Success!");
 });
+
+var tableString = "" ;
+
+//div for the table so that it doesn't lose its place when being reset
+var divTableString = `<div class="tableDiv"></div>`;
+
+function createTable () {
+    tableString= `<table class="placesList">`;
+    
+    //first row for showing the different categories
+    tableString += "<tr>";
+        tableString += "<th>Name</th>";
+        tableString += "<th>Owned By</th>";
+        tableString += "<th>Place Type</th>";
+        tableString += "<th>Address</th>";
+        tableString += "<th>Neighborhood</th>";
+        tableString += "<th>Square Feet</th>";
+        tableString += "<th>Has a garage</th>";
+        tableString += "<th>Is reachable by transit</th>";
+    tableString += "</tr>";
+
+    //loop to set up each entry and put them in their respecitve cells
+    for(let i = -1; i < placesCount; i++) {
+        tempPlace = JSON.parse(sessionStorage.getItem(`place${i + 1}`));
+
+        tableString += "<tr>";
+            tableString += `<td id="entry${i}Name" class="entry${i}">${tempPlace.name}</td>`;
+            tableString += `<td id="entry${i}Owner" class="entry${i}">${tempPlace.ownedBy}</td>`;
+            tableString += `<td id="entry${i}PlaceType" class="entry${i}">${tempPlace.placeType}</td>`;
+            tableString += `<td id="entry${i}Address" class="entry${i}">${tempPlace.address}</td>`;
+            tableString += `<td id="entry${i}Neighborhood" class="entry${i}">${tempPlace.neighborhood}</td>`;
+            tableString += `<td id="entry${i}SqFt" class="entry${i}">${tempPlace.sqft}</td>`;
+            tableString += `<td id="entry${i}Garage" class="entry${i}">${tempPlace.garage}</td>`;
+            tableString += `<td id="entry${i}Transit" class="entry${i}">${tempPlace.transitReachable}</td>`;
+        tableString += "</tr>";
+    }
+
+    //once everything has been recorded on the tableString, put the ending table tag to complete the element
+    tableString += "</table>";
+
+    //for debug/console check to make sure everything is being written properly
+    console.log(tableString);
+}
