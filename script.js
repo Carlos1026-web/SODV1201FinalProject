790390a987bb4e983683cf4599fbff1884750d4f
Carlos1026-web <Derek.carlos04@gmail.com>
2025-03-21 18:01:19 -0600

    working on getting current user to show up on onload function for createNewPlace

--- script.js
+++ script.js
@@ -11,10 +11,15 @@ var placesCount = 0;
 
 function loadUserNum() {
     if(sessionStorage.getItem("usersCount")!=0)
-        {
-            usersCount = Number(sessionStorage.getItem("usersCount"));
-            alert(`number of users: ${usersCount}`);
-        }
+    {
+        usersCount = Number(sessionStorage.getItem("usersCount"));
+        alert(`number of users: ${usersCount}`);
+    }
+    
+    if(currentUser.getItem("currentUserName")!="") {
+        currentUser = sessionStorage.getItem("currentUserName");
+        alert(`current user: ${currentUser}`);
+    }
 }
 
 $("#signUpForm").on("submit", function() {
@@ -83,33 +88,42 @@ $("#logInForm").on("submit", function() {
 });
 
 $("#newPlaceForm").on("submit", function() {
-    let logInEmail = document.getElementById("logInUserEmail").value ;
-    let logInPassword = document.getElementById("logInPassword").value;
+    let name = document.getElementById("placeName").value ;
+    let address = document.getElementById("placeAddress").value;
+    let neighborhood = document.getElementById("placeNeighborhood").value;
+    let sqft = document.getElementById("placeSquareFeet").value;
+    let garage = document.getElementById("placeGarage").value;
+    let transitReachable = document.getElementById("placeReachablePublicTransit").value;
+    let placeType = document.getElementById("placeType").value;
 
-    if(usersCount > 0) {
+    alert(`name: ${name}
+    \naddress: ${address}
+    \nneighborhood: ${neighborhood}
+    \nsqft: ${sqft}
+    \ngarage: ${garage}
+    \ntransitReachable: ${transitReachable}
+    \nplaceType: ${placeType}`
+    );
 
-        for(let i = -1; i < usersCount; i++) {
-            // let tempUser = sessionStorage.getItem(`user${i + 1}`);
-            let tempUser = JSON.parse(sessionStorage.getItem(`user${i + 1}`));
-            alert(`user${i + 1} is current scanning user`);
-
-            alert(`${tempUser.email}\n${tempUser.password}\n${tempUser.name}\n${tempUser.phone}\n${tempUser.role}`);
-
-            if(logInEmail == tempUser.email && logInPassword == tempUser.password) {
-                alert("Log In Successful!");
-                currentUser = tempUser.name;
-                sessionStorage.setItem('currentUserName', currentUser);
-                break;
-            }
-            else if (logInPassword != tempUser.password) {    //debugging purposes ONLY
-                alert("Password is not correct!");
-            }
-            else {
-                alert("Incorrect Credentials");
-            }
-        }
+    let newPlace = {
+        "ownedBy": currentUser,
+        "name": email,
+        "address": address,
+        "neighborhood": neighborhood,
+        "sqft": sqft,
+        "garage": garage,
+        "transitReachable": transitReachable,
+        "placeType": placeType
     }
-    else {
-        alert("No users in the database!")
-    }
+
+    //sessionStorage.setItem(`user${usersCount}`, newUser);
+    sessionStorage.setItem(`place${placesCount}`, JSON.stringify(newPlace));
+    placesCount++;
+    sessionStorage.setItem('placesCount', placesCount);
+
+    // usersArray.push(newUser);
+
+    alert(`${newPlace.name}\n${newPlace.address}\n${newPlace.neighborhood}\n${newPlace.sqft}\n${newPlace.garage}\n${newPlace.transitReachable}\n${newPlace.placeType}`);
+
+    $("#signUpMsg").html("Sign Up Success!");
 });
